0a402881488e5f20ce07551c3435b98694c820d5
adamczi <none>
2017-08-16 06:53:41 +0200

    rm old file

--- app/models.py
+++
@@ -1,9 +0,0 @@
-import pg_simple
-from config import host, port, database
-
-
-pool = pg_simple.config_pool(host=host,
-                           port=port,
-                           database=database,
-                           user="docker",
-                           password="docker")
