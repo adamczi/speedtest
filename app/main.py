a80612e239815eea255f13b93b965a5be67ca174
adamczi <none>
2017-08-16 06:52:57 +0200

    user authentication

--- app/main.py
+++ app/main.py
@@ -1,73 +1,82 @@
 import sys
-from flask import Flask, render_template, send_file, flash, session, url_for
-from config import key_secret
-from models import pool
+from flask import Flask, render_template, session, send_file, url_for, \
+request, abort, redirect
+from config import key_secret, userTable
+from utils import messages, validate, loggedIn
+from db import pool, db
+import os
 import re
-import psycopg2, pg_simple
+import psycopg2
+import pg_simple
+from flask_bcrypt import Bcrypt
 from datetime import datetime
 
 
-## App start
+# App start
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-# Function for message flashing
-def messages(message):
-    print(message)
-    flash(message)
-    return
-
-# Wrapper for data validation and parsing
-def validate(func):
-    def wrapper(*args):
-        # try:
-        values = re.findall('(\.*[0-9]+\.[0-9]+)',args[1])
-        for i in xrange(3):
-            values[i] = values[i][1:] if values[i].startswith('.') else values[i]
-        # except Exception as e:
-        return func(args[0],values)
-    return wrapper
+bcrypt = Bcrypt(app)
 
 
 # just a random mainpage
 @app.route("/")
 def itWorks():
+    # with pg_simple.PgSimple() as db:
+    #     db.insert("users",
+    #               {"name": 'marcin',
+    #                "password": 'asdf',
+    #                "api": '1'})
+    #     db.commit()
     return "aXQgd29ya3Mh=="
 
 
 # route to login page
-@app.route("/login")
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
+    username = request.form['username']
+    try:
+        password = db.fetchone(userTable,
+                   fields=['password'],
+                   where=('name = %s', [username]))[0]
+        # if bcrypt.check_password_hash(password, request.form['password']):
+        if password == request.form['password']:
+            session['username'] = request.form['username']
+            return redirect(url_for('stats'))
 
-# @app.route("/postlogin", methods=["GET", "POST"])
-# def postlogin():
-#     if request.method = "POST":
+    except TypeError as e:
+       pass
+
+    messages("401")
+    return redirect(url_for('login'))
+
+
+@app.route('/logout')
+@loggedIn
+def logout():
+    session.clear()
+    return 'Logged out'
 
 
 # the API route to call from your script
 @app.route("/record/<timestamp>/<speedData>")
 def getValues(timestamp, speedData):
-    # parse the values
-
-    # createGraph(speedData)
 
     # save into the DB
     saveToDatabase(timestamp, speedData)
 
-    # immediately load new up to date values
-    # loadToGraph()
-
-    return "It works!"
+    return "it works!"
 
 
 # Funtion which saves the data
 @validate
 def saveToDatabase(timestamp, speedData):
     # save to Postgres
-    # insert into data values(1, (SELECT to_timestamp('07/08/2017,18:00:40', 'DD-MM-YYYY,hh24:mi:ss')::timestamp without time zone), 26.756, 23.4, 2.53);
-    print timestamp
-    print speedData
+    # insert into data values(1, (SELECT to_timestamp('07/08/2017,18:00:40',
+    # 'DD-MM-YYYY,hh24:mi:ss')::timestamp without time zone),
+    # 26.756, 23.4, 2.53);
+
     with pg_simple.PgSimple() as db:
         db.insert("data",
                   {"date": timestamp,
@@ -77,13 +86,15 @@ def saveToDatabase(timestamp, speedData):
         db.commit()
     return
 
-# Function which loads data for given user and passes it to draw the graph
-def loadToGraph():
-    # load from postgres, jsonify and pass to html
-    return
 
-# random graph shows up here
+@app.route("/stats")
+@loggedIn
+def stats():
+    return render_template('stats.html')
+
+
 @app.route('/index')
+@loggedIn
 def index():
     return send_file('./static/index.html')
 
---
+++ app/utils.py
@@ -0,0 +1,28 @@
+from functools import wraps
+from flask import flash, redirect, url_for, session
+
+# Function for message flashing
+def messages(message):
+    print(message)
+    flash(message)
+    return
+
+
+# Decorator for data validation and parsing
+def validate(func):
+    def wrapper(*args):
+        values = re.findall('(\.*[0-9]+\.[0-9]+)', args[1])
+        for i in xrange(3):
+            values[i] = values[i][1:] if values[i].startswith('.') else values[i]
+        return func(args[0], values)
+    return wrapper
+
+
+# Decorator for user authentication
+def loggedIn(func):
+    @wraps(func)
+    def loginCheck(*args):
+        if 'username' not in session:
+            return redirect(url_for('login'))
+        return func(*args)
+    return loginCheck
