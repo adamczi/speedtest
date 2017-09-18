0cc985b962194ff458ed43d1d8e9ae89800ae5b3
adamczi <adamczi@users.noreply.github.com>
2017-09-18 23:13:35 +0200

    vue computing stats

--- app/static/v_app.js
+++ app/static/v_app.js
@@ -1,24 +1,51 @@
 window.onload = function () {
 
-    new Vue({
+    var numbers = Vue.component('numbers', {
+        props: ['table'],
+
+        data: function () {
+            var sum = 0;
+            for (var i=0; i < this.table.length; i++) {
+                sum += this.table[i][1]
+            };
+            var result = Math.round(sum/i * 1000) / 1000;
+
+            return {
+                average: result,
+                count: i,
+                firstDate: {}, //compute max and min of this.table's timestamps
+                lastDate: {}
+            };
+        }
+    })
+
+
+    var numbersaverage = numbers.extend({
+        template: '<span>{{average}}</span>'
+    })
+    Vue.component('numbersaverage', numbersaverage)
+
+
+    var numberscount = numbers.extend({
+        template: '<span>{{count}}</span>'
+    })
+    Vue.component('numberscount', numberscount)
+
+
+    var firstdate = numbers.extend({
+        template: '<span>{{firstDate}}</span>'
+    })
+    Vue.component('firstdate', firstdate)
+
+
+    var lastdate = numbers.extend({
+        template: '<span>{{lastDate}}</span>'
+    })
+    Vue.component('lastdate', lastdate)
+
+
+    var app = new Vue ({
         el: '#speeds',
-        data: {
-            average: {},
-            records: {}
-        },
-        methods: {
-            setJson (payload) {
-                var sum = 0;
-                for (var i=0; i < payload.length; i++) {
-                    sum += payload[i][1]
-                };
-                result = sum/i;
-                this.average = Math.round(result * 1000) / 1000;
-                this.records = i;
-            },
-        },
-
-        delimiters: ['[[',']]'],
+        delimiters: ['[[',']]']
     });
-
 }
